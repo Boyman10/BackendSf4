47909f5cf130bf18b7b43528bad881f59d4a0f6a
Boyman10 <contact@yasi.fr>
2018-04-29 12:02:40 +0200

    Adding new admin user - should be implementing password encryption asap

--- db/01_inserts.sql
+++ db/01_inserts.sql
@@ -1,3 +1,5 @@
 insert into role set role_name = "administrator";
 insert into role set role_name = "visitor";
 insert into role set role_name = "editor";
+
+insert into member set role_id = 1, pass = "test", username = "boy";
