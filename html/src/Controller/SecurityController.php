7b5cb3648871a213954d515ae8c77b958a9dd6c6
Boyman10 <contact@yasi.fr>
2018-04-29 16:54:06 +0200

    Providing proper interfaces to be able to use the controller with Request and route annotations

--- html/src/Controller/SecurityController.php
+++ html/src/Controller/SecurityController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
