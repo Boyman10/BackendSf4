6a44dc14ca09e21ea5684b585c208a8f7ab6e37c
Boyman10 <contact@yasi.fr>
2018-05-19 15:38:02 +0200

    Updating injection mecanism into RegisterClient - using services.yaml configuration to properly inject dependencies in constructor

--- html/src/Action/RegisterClient.php
+++ html/src/Action/RegisterClient.php
@@ -19,50 +19,84 @@ use Symfony\Component\Routing\Annotation\Route;
  * Date: 01/05/18
  * Time: 17:51
  */
-class RegisterClient
+final class RegisterClient
 {
+
     /**
-     * New Route to let the user fill in details about a client
-     * @Route("/newclient", name="client_registration")
-     * @Method({"GET", "POST"})
+     * @var $responder
+     */
+    private $responder;
+    /**
+     * @var $formFactory
+     */
+    private $formFactory;
+    /**
+     * @var $doctrine
+     */
+    private $doctrine;
+    /**
+     * @var $logger
+     */
+    private $logger;
+    /**
+     * @var $request
+     */
+    private $request;
+    /**
+     * RegisterClient constructor.
      * @param RegisterResponderInterface $responder
      * @param FormFactoryInterface $formFactory
      * @param ManagerRegistry $doctrine
      * @param LoggerInterface $logger
      * @param Request $request
+     */
+    public function __construct(RegisterResponderInterface $responder,
+                                Request $request, LoggerInterface $logger,
+                                FormFactoryInterface $formFactory,
+                                ManagerRegistry $doctrine)
+    {
+        $this->responder = $responder;
+        $this->logger = $logger;
+        $this->doctrine = $doctrine;
+        $this->formFactory = $formFactory;
+        $this->request = $request;
+    }
+
+    /**
+     * New Route to let the user fill in details about a client
+     * @Route("/newclient", name="client_registration")
+     * @Method({"GET", "POST"})
      * @return \Symfony\Component\HttpFoundation\Response $twig
      */
-    public function __invoke(Request $request, LoggerInterface $logger,
-                             RegisterResponderInterface $responder,
-                             FormFactoryInterface $formFactory, ManagerRegistry $doctrine) : Response
+    public function __invoke() : Response
     {
 
         // 1) build the form
         $client = new Client();
 
-        $form = $formFactory->create(NewClientForm::class, $client);
+        $form = $this->formFactory->create(NewClientForm::class, $client);
 
         // 2) handle the submit (will only happen on POST)
-        $form->handleRequest($request);
+        $form->handleRequest($this->request);
 
         // Handle the form submission
         if ($form->isSubmitted() && $form->isValid()) {
 
-            $logger->info("Form is valid, we persist data to DB");
+            $this->logger->info("Form is valid, we persist data to DB");
 
             // 4) save the User and addresses !
-            $entityManager = $doctrine->getManager();
+            $entityManager = $this->doctrine->getManager();
 
             $entityManager->persist($client);
             $entityManager->flush();
 
             // Return to view with message
-            return $responder(['message' => "Good on you, the client is added ! Now check the details -- @TODO NEXT"]);
+            return $this->responder(['message' => "Good on you, the client is added ! Now check the details -- @TODO NEXT"]);
         }
 
         $data = ['form' => $form->createView()];
         // Return the new response from responder :
 
-        return $responder($data);
+        return $this->responder($data);
     }
 }
